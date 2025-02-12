f18e7e1459b8d3e03646d8362c17fceb25055938
Tiago Gomes <tidgomes@gmail.com>
2025-02-12 12:30:49 -0300

    implement EmployeeDAO

---
+++ src/main/java/com/tidz/crud_demo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,26 @@
+package com.tidz.crud_demo.dao;
+
+import com.tidz.crud_demo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = this.entityManager.createQuery("FROM Emplyee", Employee.class);
+        return query.getResultList();
+    }
+}
