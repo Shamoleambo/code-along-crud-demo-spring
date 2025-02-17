28d448181c2127ba38738e271308c353a620ea70
Tiago Gomes <tidgomes@gmail.com>
2025-02-17 16:38:36 -0300

    enable route to get single employee by id

--- src/main/java/com/tidz/crud_demo/rest/EmployeeRestController.java
+++ src/main/java/com/tidz/crud_demo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.tidz.crud_demo.entity.Employee;
 import com.tidz.crud_demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return this.employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return this.employeeService.findById(id);
     }
 }
