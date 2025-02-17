df7d765c651a26036963aeaaf28d746fabde9681
Tiago Gomes <tidgomes@gmail.com>
2025-02-17 15:56:25 -0300

    fix missing annotation

--- src/main/java/com/tidz/crud_demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/tidz/crud_demo/service/EmployeeServiceImpl.java
@@ -2,13 +2,17 @@ package com.tidz.crud_demo.service;
 
 import com.tidz.crud_demo.dao.EmployeeDAO;
 import com.tidz.crud_demo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeDAO employeeDAO;
 
+    @Autowired
     public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
